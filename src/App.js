f60b140b203b14627ee65bb60dad166dd16d5066
haileyeong <haileyeong@gmail.com>
2024-09-29 02:13:13 +0900

    Style: 요소 스타일 변경

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SearchBar from './components/SearchBar';
 function App() {
   return (
     <div style={{ position: 'relative' }}>
-      <SearchBar style={{ position: 'absolute', top: '20px', left: '20px', zIndex: 10 }} /> {/* SearchBar 위치 조정 */}
+      <SearchBar style={{ position: 'absolute', top: '20px', left: '20px', zIndex: 10 }} />
       <Map style={{ flex: 1 }} /> {/* Map이 가능한 모든 공간을 차지하도록 설정 */}
     </div>
   );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+// import SearchBar from './components/SearchBar';
 
 const Map = () => {
     useEffect(() => {
